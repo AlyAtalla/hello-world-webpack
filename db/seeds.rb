c2122a0c788230e929e79894354eb51297662dab
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-04 11:03:25 +0200

    fix linter errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 Message.destroy_all
 
 greetings = [
-  "Hello, World!",
-  "Welcome to the app!",
-  "Greetings from Rails!",
-  "Hola, amigos!",
-  "Good day to you!"
+  'Hello, World!',
+  'Welcome to the app!',
+  'Greetings from Rails!',
+  'Hola, amigos!',
+  'Good day to you!'
 ]
 
 greetings.each do |greeting|
