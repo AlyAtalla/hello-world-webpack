1ca74a38d32003a6ae5fc44d5dfdfac427a29f31
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-04 10:59:36 +0200

    update app.js

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchRandomGreeting } from '../actions/greetingActions';
 import Greeting from './Greeting';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Fetch random greeting when the app component mounts
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
   return (
     <Router>
       <Route path="/" exact component={Greeting} />
