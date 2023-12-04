1cfb82fc869a62e2be05d5a5554438e5c5274e31
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-04 22:10:06 +0200

    reinstall jswebpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,20 @@
-const path = require("path");
-const webpack = require("webpack");
+const path    = require("path")
+const webpack = require("webpack")
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js",
+    application: "./app/javascript/application.js"
   },
   output: {
     filename: "[name].js",
+    sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
-    publicPath: "/assets/",
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
-  ],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-    ],
-  },
-};
+      maxChunks: 1
+    })
+  ]
+}
