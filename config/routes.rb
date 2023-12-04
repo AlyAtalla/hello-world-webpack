c45ff7309e6e5b7c05ad88dd5b9299f21f2beee3
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-04 10:32:44 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  get '/random_greeting', to: 'greetings#random_greeting'
   # root "posts#index"
 end
