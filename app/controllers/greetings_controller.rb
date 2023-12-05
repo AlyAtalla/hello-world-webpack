7fcb28e01a2eae6cbea4aa705b6400620069f104
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-05 12:39:48 +0200

    update greetings controller

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -4,23 +4,3 @@ class GreetingsController < ApplicationController
     render json: @greeting
   end
 end
-greeting.j
-import React, { useEffect } from 'react';
-import { getGreetingsAsync } from '../reducers/greetingReducer';
-import { useSelector, useDispatch } from 'react-redux';
-const Greeting = () => {
-  const { greeting, isLoading, error } = useSelector((state ) => state.greetings)
-  const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(getGreetingsAsync())
-  }, [dispatch])
-  return (
-    <div>
-      <h1>Random Greeting:</h1>
-      {isLoading && <h2>Loading...</h2>}
-      {error && <h2>{error?.message}</h2>}
-      {greeting && <p>{greeting.content}</p>}
-    </div>
-  );
-};
-export default Greeting;
