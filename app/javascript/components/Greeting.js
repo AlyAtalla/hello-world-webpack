10bd142ca5bcfb8622fb187448d98f8fdeab8584
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-05 11:46:34 +0200

    update greetings.js

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,15 +1,19 @@
-import React from 'react';
-
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRandomGreeting } from '../actions/greetingActions';
 const Greeting = () => {
-  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRandomGreeting());
+  }, [dispatch]);
+
+  const randomGreeting = useSelector(state => state.greeting.randomGreeting);
 
   return (
     <div>
       <h1>Random Greeting:</h1>
-  
+      <p>{randomGreeting}</p>
     </div>
   );
 };
-
-export default Greeting;
