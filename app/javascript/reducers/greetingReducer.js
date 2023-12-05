9652c65a6933011b2c60e2607a063b9101bce6b8
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-12-05 11:32:19 +0200

    update greeting rudecer

--- app/javascript/reducers/greetingReducer.js
+++ app/javascript/reducers/greetingReducer.js
@@ -1,17 +1,49 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+// Initial state
 const initialState = {
   randomGreeting: '',
+  status: 'idle',
+  error: null
 };
 
-const greetingReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'SET_RANDOM_GREETING':
-      return {
-        ...state,
-        randomGreeting: action.payload,
-      };
-    default:
-      return state;
+// Async thunk
+export const fetchRandomGreeting = createAsyncThunk(
+  'greeting/fetchRandomGreeting',
+  async (_, { rejectWithValue }) => {
+    try {
+      // Replace with your async request
+      const response = await fetch('https://api.example.com/greeting');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data.greeting;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
   }
-};
+);
 
-export default greetingReducer;
+// Slice
+const greetingSlice = createSlice({
+  name: 'greeting',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRandomGreeting.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchRandomGreeting.fulfilled, (state, { payload }) => {
+        state.status = 'idle';
+        state.randomGreeting = payload;
+      })
+      .addCase(fetchRandomGreeting.rejected, (state, { payload }) => {
+        state.status = 'idle';
+        state.error = payload;
+      });
+  }
+});
+
+export default greetingSlice.reducer;
